b73cb4ab1437f8372f618b88888b0ed7a975df77
Jonathan Reider <reiderj8@gmail.com>
2024-11-21 16:25:38 -0500

    Updated to query timestream db

--- sir-results-management/test_time_stream.py
+++ sir-results-management/test_time_stream.py
@@ -3,14 +3,16 @@ import unittest
 import time_stream_interface
 from datetime import datetime
 
+# Note that tests will fail if AWS credentials is not setup.
 class TestTimeStream(unittest.TestCase):
 
-   def test_write(self):
+   def test_read_after_write(self):
       # Setup environment variables
       os.environ['DATABASE_NAME'] = 'sir-sim-results'
       os.environ['TABLE_NAME'] = 'results'
 
       timeStream = time_stream_interface.timeStreamHandler('us-east-1')
+      start_time = datetime.now().timestamp()  * 1000
 
       result = {
          'time': int(datetime.now().timestamp() * 1000),
@@ -23,7 +25,13 @@ class TestTimeStream(unittest.TestCase):
          'results' : [result]
       }
 
-      timeStream.writeResults(dailyResults)
+      success = timeStream.writeResults(dailyResults)
+      self.assertTrue(success)
+
+      read_results = timeStream.readResults(start_time - 1000, datetime.now().timestamp()  * 1000)
+      self.assertEqual(1, read_results['results'][0]['numSusceptible'])
+      self.assertEqual(2, read_results['results'][0]['numInfected'])
+      self.assertEqual(3, read_results['results'][0]['numRecovered'])
 
 
 if __name__ == '__main__':
--- sir-results-management/time_stream_interface.py
+++ sir-results-management/time_stream_interface.py
@@ -4,13 +4,20 @@ import boto3
 from botocore.config import Config
 from datetime import datetime, timezone
 
+# sources: 
+#  https://github.com/awslabs/amazon-timestream-tools/blob/mainline/sample_apps/python/SampleApplication.py
+#  https://docs.aws.amazon.com/timestream/latest/developerguide/code-samples.run-query.html
 class timeStreamHandler:
 
    def __init__(self, region):
       self.write_client = boto3.Session().client('timestream-write', 
                                                  config=Config(region_name = region, read_timeout =20, 
                                                                max_pool_connections = 5000, retries={'max_attempts': 10}))
+      self.query_client = boto3.Session().client('timestream-query', config=Config(region_name = region))
       self.dimensions = [ {'Name': 'region', 'Value': region} ]
+      self.database_name = DatabaseName=os.environ["DATABASE_NAME"]
+      self.table_name = DatabaseName=os.environ["TABLE_NAME"]
+
 
    def writeResults(self, simResults):
 
@@ -18,7 +25,7 @@ class timeStreamHandler:
 
       for result in simResults['results']:
 
-         print(result)
+         # print(result)
 
          # Convert date time to milliseconds epoch as a string
          time = str(result['time'])
@@ -54,7 +61,7 @@ class timeStreamHandler:
          result = self.write_client.write_records(DatabaseName=os.environ["DATABASE_NAME"], TableName=os.environ["TABLE_NAME"],
                   Records=writeRecords, CommonAttributes={})
          print("WriteRecords Status: [%s]" % result['ResponseMetadata']['HTTPStatusCode'])
-         success = True
+         success = result['ResponseMetadata']['HTTPStatusCode'] == 200
       except self.write_client.exceptions.RejectedRecordsException as err:
          self._print_rejected_records_exceptions(err)
       except Exception as err:
@@ -62,4 +69,89 @@ class timeStreamHandler:
 
       return success
 
+   # Source: https://github.com/awslabs/amazon-timestream-tools/blob/mainline/sample_apps/python/QueryExample.py
+   # Start and End time are milliseconds epoch
+   def readResults(self, startTime, endTime):
+      query = \
+         "WITH Results AS ( " \
+         "SELECT time, measure_name, measure_value::bigint as value " \
+         "FROM \"" + self.database_name + "\".\""+ self.table_name + "\") " \
+         "SELECT infected.time, infected.value AS numInfected, susceptible.value AS numSusceptible, recovered.value AS numRecovered " \
+         "FROM Results infected " \
+         "LEFT JOIN Results susceptible ON infected.time = susceptible.time AND susceptible.measure_name = 'num_susceptible' " \
+         "LEFT JOIN Results recovered ON infected.time = recovered.time AND recovered.measure_name = 'num_recovered' " \
+         "WHERE infected.measure_name = 'num_infected' " \
+         "AND infected.time between from_milliseconds(" + str(round(startTime)) + ") and from_milliseconds(" + str(round(endTime)) + ")" \
+         "ORDER BY infected.time DESC"
+      
+      read_data = []
+
+      try:
+         iterator = self.query_client.get_paginator('query').paginate(QueryString=query)
+         for page in iterator:
+            column_info = page['ColumnInfo']
+            for row in page['Rows']:
+               data = self._parse_row(column_info, row)
+               read_data.append(data)   
+      except Exception as err:
+         print("Failed to query database", err)
+      
+      results = { 
+         'results' : read_data 
+      }
+
+      return results
    
+   # Following is a generic parsing from https://docs.aws.amazon.com/timestream/latest/developerguide/code-samples.run-query.html
+   def _parse_row(self, column_info, row):
+      data = row['Data']
+      row_output = {}
+      for j in range(len(data)):
+         info = column_info[j]
+         datum = data[j]   
+         key, value = self._parse_datum(info, datum)
+         row_output[key] = value
+
+      return row_output
+   
+   def _parse_datum(self, info, datum):
+      if datum.get('NullValue', False):
+         print("Error - null value was queried")
+         return "", 0
+
+      column_type = info['Type']
+
+      # If the column is of Scalar Type
+      if 'ScalarType' in column_type:
+         return self._parse_column_name(info), self._convert_value(info, datum['ScalarValue'])
+      else:
+         print("Error - Invalid input type queried")
+         return "", 0
+   
+   def _parse_column_name(self, info):
+      if 'Name' in info:
+         return info['Name']
+      else:
+         print("Error - Queried column has no name")
+         return ""
+      
+   def _convert_value(self, info, str_value):
+      column_type = info['Type']
+      #print("Converting value type " + column_type + ", value: " + str_value)
+      if 'ScalarType' in column_type:
+         format = column_type['ScalarType']
+         if(format == 'TIMESTAMP'):
+            format_str = '%Y-%m-%d %H:%M:%S.%f'
+            # Remove nano-second portion of string
+            time_str = str_value[:-3]
+            datetime_obj = datetime.strptime(time_str, format_str)
+            # return epoch milliseconds
+            return int(datetime_obj.timestamp() * 1000)
+         elif(format == 'BIGINT'):
+            return int(str_value)
+         else:
+            # Not supported type
+            return str_value
+      else:
+         # Non-scalar values not supported at this time
+         return str_value
