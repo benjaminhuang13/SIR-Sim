111ecbbdbb0d4b890f5d51436f3c9b04a7b18ac1
Benjamin Huang <benjamin.huang@alumni.stonybrook.edu>
2024-11-30 17:27:19 -0500

    add try catch and check if body is empty

--- sir-engine/lambda_handler.py
+++ sir-engine/lambda_handler.py
@@ -8,40 +8,55 @@ sqs = boto3.client('sqs')
 OUTPUT_QUEUE_URL = os.environ.get('OUTPUT_QUEUE', 'errorVal')
 
 def lambda_handler(event, context):
-    results = []
-    for record in event['Records']:
-        body = json.loads(record['body'])  # Parse the message body
-        user_inputs = body.get('userInputs')
-
-        try:
-            population_size = user_inputs['populationSize']
-            infection_rate = user_inputs['infectionRate']
-            num_infected = user_inputs['numInfected']
-            recovery_rate = user_inputs['recoveryRate']
-            time_steps = user_inputs['timeStepsDays']
-        except:
-            if user_inputs:
-                result = { "results": f"malformed input: {user_inputs}" }
-                results.append(result)
+    try:
+        results = []
+        for record in event['Records']:
+            body = json.loads(record['body'])  # Parse the message body
+            if not body:
+                print(f"Warning: Empty body in record: {record}")
                 continue
-            else:
-                result = { "results": f"malformed input: key 'user_inputs' not found in input json" }
-                results.append(result)
-                continue
+            user_inputs = body.get('userInputs')
 
-        # Run SIR simulation
-        sim_results = sir_simulation(population_size, infection_rate, num_infected, recovery_rate, time_steps)
-        result = { "results": sim_results }
-        results.append(result)
+            try:
+                population_size = user_inputs['populationSize']
+                infection_rate = user_inputs['infectionRate']
+                num_infected = user_inputs['numInfected']
+                recovery_rate = user_inputs['recoveryRate']
+                time_steps = user_inputs['timeStepsDays']
+            except:
+                if user_inputs:
+                    result = { "results": f"malformed input: {user_inputs}" }
+                    results.append(result)
+                    continue
+                else:
+                    result = { "results": f"malformed input: key 'user_inputs' not found in input json" }
+                    results.append(result)
+                    continue
 
-        # Send the result to the output SQS queue
-        sqs.send_message( QueueUrl=OUTPUT_QUEUE_URL, MessageBody=json.dumps(result) )
+            # Run SIR simulation
+            sim_results = sir_simulation(population_size, infection_rate, num_infected, recovery_rate, time_steps)
+            result = { "results": sim_results }
+            results.append(result)
 
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Origin': '*',  
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
-            },
-        'body': json.dumps({'message': 'Successful execution'})
-        }
+            # Send the result to the output SQS queue
+            sqs.send_message( QueueUrl=OUTPUT_QUEUE_URL, MessageBody=json.dumps(result) )
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',  
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+                },
+            'body': json.dumps({'message': 'Successful execution'})
+            }
+    except Exception as e:        
+        print('something wrong happened: {}'.format(e))
+        return {
+            'statusCode': 204,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',  
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+                },
+            'body': json.dumps({'message': 'something wrong happened, check logs'})
+            }
+    
