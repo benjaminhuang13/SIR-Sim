0e4f8148d05fef70b89a799fdee245794b460af1
Benjamin Huang <benjamin.huang@alumni.stonybrook.edu>
2024-12-01 17:01:35 -0500

    debug

--- sir-results-management/lambda_handler.py
+++ sir-results-management/lambda_handler.py
@@ -5,30 +5,41 @@ import time_stream_interface
 import logging
 
 def lambda_handler(event, context):
+   try:
+      logging.basicConfig(level=logging.INFO) 
+      logger = logging.getLogger()
 
-   logging.basicConfig(level=logging.INFO) 
-   logger = logging.getLogger()
+      region = os.environ["AWS_REGION"]
 
-   region = os.environ["AWS_REGION"]
+      # TODO - I don't believe we are doing batch lambdas for this.
+      #  May need to add a loop through the records
+      print("event: {}".format(event))
+      results =  event['Records'][0]['body']
+      results_json = json.loads(json.loads(results))
+      print(type(results_json))
+      print(type(results))
+      print('parsed str: {}'.format(results_json))
 
-   # TODO - I don't believe we are doing batch lambdas for this.
-   #  May need to add a loop through the records
-   results_json =  event['Records'][0]['body']
-   results = json.loads(results_json)
+      success, errMessage = time_stream_interface.writeResults(region, results_json)
 
-   success, errMessage = time_stream_interface.writeResults(region, results)
-
-   if success:
-      # If successful send event to successful write queue
-      sqs_handler.sendSuccessResults(region, results_json)
-
+      if success:
+         # If successful send event to successful write queue
+         sqs_handler.sendSuccessResults(region, results_json)
+         print("Results successfully written: {}".format(results_json))
+         return {
+            'statusCode': 200,
+            'body': 'Results successfully written'
+         }
+      else:
+         logger.error("Failed to write to Timestream: " + errMessage)
+         print("Failed to write to Timestream: " + errMessage)
+         return {
+            'statusCode': 500,
+            'body': errMessage
+         }
+   except Exception as e:
+      print("Error: {}".format(e))
       return {
-         'statusCode': 200,
-         'body': 'Results successfully written'
-      }
-   else:
-      logger.error("Failed to write to Timestream: " + errMessage)
-      return {
-         'statusCode': 500,
-         'body': errMessage
-      }
+            'statusCode': 500,
+            'body': e
+         }
