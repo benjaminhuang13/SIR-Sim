9ee0a581bfc6fbb6482618b3da95fd89384981d5
Benjamin Huang <benjamin.huang@alumni.stonybrook.edu>
2024-12-03 22:20:38 -0500

    remove folder

--- sir-results-java/src/main/java/edu/jhu/sir_results_management/IEventQueueHandler.java
+++
@@ -1,6 +0,0 @@
-package edu.jhu.sir_results_management;
-
-public interface IEventQueueHandler {
-   void handleSimResults(String results);
-   void handleExportResults(String results);
-}
--- sir-results-java/src/main/java/edu/jhu/sir_results_management/IResultsStorage.java
+++
@@ -1,8 +0,0 @@
-package edu.jhu.sir_results_management;
-
-import edu.jhu.Messages.SimResults;
-
-public interface IResultsStorage {
-
-    void storeResults(SimResults results);
-}
